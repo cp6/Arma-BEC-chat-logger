920410aa0bc89786f7b08428fe07c494e6ea2fc3
cp6 <cp6@users.noreply.github.com>
2018-07-27 10:51:42 +1000

    Update display.php

--- display.php
+++ display.php
@@ -27,8 +27,9 @@
         }
     </style>
 </head>
+    <body>
 <?php
-$connect = mysqli_connect("localhost", "root", "", "bec_chat");//MYSQL details
+$connect = mysqli_connect("localhost", "USERNAME", "PASSWORD", "bec_chat");//MYSQL details
 $date = date('Y-m-d H:i:s');
 //DISPLAY SECTION
 $sql = "SELECT `player`, `message`, `date` FROM `server1` WHERE `type` = 0 ORDER BY `id` DESC LIMIT 250;";
@@ -38,7 +39,8 @@ while ($row = $result->fetch_assoc()) {
     $message = $row["message"];
     $db_date = $row["date"];
     $datetime = date("g:i:s A D jS M", strtotime($db_date));
-    echo "<body><p class='pink'>$player</p> <p class='green'>:</p> <p class='blue'>$message</p> <p class='orange'>$datetime</p><br></body>";
+    echo "<p class='pink'>$player</p> <p class='green'>:</p> <p class='blue'>$message</p> <p class='orange'>$datetime</p><br>";
 };
 ?>
+        </body>
 </html>
