756afa48d9830a85357631988b8d055cd4d33116
cp6 <cp6@users.noreply.github.com>
2021-10-14 11:25:00 +1100

    Optimized code
    Used str_contains
    
    merged str_replace

--- log.php
+++ log.php
@@ -1,28 +1,27 @@
 <?php
 $mysql_connect = mysqli_connect("localhost", "USERNAME", "PASSWORD", "bec_chat");//MySQL details
 
-function chatLogFile($filename, $newest_first = false)
+function chatLogFile(string $filename, bool $newest_first = false): array
 {
     if ($newest_first) {
         return array_reverse(file($filename));//The newest message first
-    } else {
-        return file($filename);//file name
     }
+    return file($filename);//file name
 }
 
-function chat_type($string): int
+function chat_type(string $string): int
 {
-    if (strpos($string, 'Side') !== false) {
+    if (str_contains($string, 'Side')) {
         return 0;
-    } elseif (strpos($string, 'Direct') !== false) {
+    } elseif (str_contains($string, 'Direct')) {
         return 1;
-    } elseif (strpos($string, 'Vehicle') !== false) {
+    } elseif (str_contains($string, 'Vehicle')) {
         return 2;
-    } elseif (strpos($string, 'Global') !== false) {
+    } elseif (str_contains($string, 'Global')) {
         return 3;
-    } elseif (strpos($string, 'Command') !== false) {
+    } elseif (str_contains($string, 'Command')) {
         return 4;
-    } elseif (strpos($string, 'Group') !== false) {
+    } elseif (str_contains($string, 'Group')) {
         return 5;
     } else {
         return 6;//Unknown type
@@ -39,16 +38,14 @@ foreach ($file as $line) {
     $arr = explode(":", substr($line, $start), 2);
     $player = $ar[4];
     if (isset($ar[6])) {
-        $one = str_replace("(", "(:", $ar[5]);
-        $one = str_replace(")", "):", $one);
-        $two = str_replace(")", "):", $ar[6]);
-        $two = str_replace("(", "(:", $two);
+        $one = str_replace(array("(", ")"), array("(:", "):"), $ar[5]);
+        $two = str_replace(array(")", "("), array("):", "(:"), $ar[6]);
         $message = "$one $two";
     } else {
         $message = $ar[5];
     }
     $select = mysqli_query($mysql_connect, "SELECT `message` FROM `server1` WHERE `type` = '" . $type . "' AND `time` = '" . $time . "'");
-    if (mysqli_num_rows($select) == 0) {//Doesnt exist yet
+    if (mysqli_num_rows($select) === 0) {//Doesnt exist yet
         $insert_q = "INSERT INTO `server1` (`type`, `time`, `player`, `message`) VALUES ('$type', '$time', '$player', '$message')";//MYSQL query
         $insert = mysqli_query($mysql_connect, $insert_q);
     }
